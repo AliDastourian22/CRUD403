ecb3c4005c57383ce4546a4f2e52007f8c227114
Ali Dastourin <alidastourian@gmail.com>
2024-04-07 20:08:28 +0300

    base page done

--- max.py
+++ max.py
@@ -1,3 +1,10 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
+
+root=Tk()
+root.title("MaxTechnology")
+root.geometry("%dx%d+%d+%d"%(800,550,10,30))
+root.configure(bg="#00003f")
+root.resizable(False,False)
+user=[]
