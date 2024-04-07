df0d9cddeef17a4a4c8a4baf09e2c4aea778c9ca
Ali Dastourin <alidastourian@gmail.com>
2024-04-07 20:23:58 +0300

    frame of entry ended

--- max.py
+++ max.py
@@ -8,3 +8,7 @@ root.geometry("%dx%d+%d+%d"%(800,550,10,30))
 root.configure(bg="#00003f")
 root.resizable(False,False)
 user=[]
+
+frame=Frame(root,width=235,height=290,border=0)
+frame.configure(bg="#00003f")
+frame.place(x=15,y=30)
